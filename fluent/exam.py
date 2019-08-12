89e6e370b8cc7129bda49072a0b0e10336fcd42c
terazawa <tera_rothe@yahoo.co.jp>
2019-08-12 23:13:02 +0900

    tuple unpacking

--- fluent/exam.py
+++ fluent/exam.py
@@ -92,3 +92,67 @@ for i in a:
 #%%
 for tshirts in ( f"{c} {s}" for c in colors for s in sizes):
     print(tshirts)
+
+#%%
+traveler_ids = [('USA', '31195855'), ('BRA', 'CE342567'), ('ESP', 'XDA205856')]
+for passport in sorted(traveler_ids):
+    print(f"{passport[0]}/{passport[1]}")
+
+#%%
+for country, _ in traveler_ids:
+    print(country)
+
+#%%
+lax_coordinates = (33.9425, -118.408056)
+city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
+
+#%%
+latitude, longitude = lax_coordinates
+latitude
+
+#%%
+longitude
+
+#%%
+divmod(20, 8)
+
+#%%
+t = (20,8)
+divmod(*t)
+
+#%%
+quotient, remainder = divmod(*t)
+quotient, remainder
+
+#%%
+import os
+_, filename = os.path.split('/home/luciano/.ssh/idrsa.pub')
+filename
+
+#%%
+a, b, *rest = range(5)
+a, b, rest
+
+#%%
+a, b, *rest = range(3)
+a, b, rest
+
+#%%
+a, b, *rest = range(2)
+a, b, rest
+
+#%%
+a, *body, c, d = range(5)
+a, body, c, d
+
+#%%
+metro_areas = [
+    ('Tokyo', ' JP ', 36.933, (35.689722, 139.691667)),
+    ('Delhi NCR', ' IN ', 21.935, (28.613889, 77.208889)),
+    ('Mexico City', ' MX ', 20.142, (19.433333, -99.133333)),
+    ('New York-Newark', ' US ', 20.104, (40.808611, -74.020386)),
+    ('Sao Paulo', ' BR ', 19.649, (-23.547778, -46.635833)),
+]
+print(f"{'-':15} | {'lat.':^9} | {'long.':^9}")
+for name, cc, pop, (latitude, longitude) in metro_areas:
+    print(f"{name:15} | {latitude:9.4f} | {longitude:9.4f}")
