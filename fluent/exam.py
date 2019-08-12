ab8ff700e9362d905b77e2c29523798013d20c41
terazawa <tera_rothe@yahoo.co.jp>
2019-08-13 08:35:12 +0900

    named tuple

--- fluent/exam.py
+++ fluent/exam.py
@@ -156,3 +156,31 @@ metro_areas = [
 print(f"{'-':15} | {'lat.':^9} | {'long.':^9}")
 for name, cc, pop, (latitude, longitude) in metro_areas:
     print(f"{name:15} | {latitude:9.4f} | {longitude:9.4f}")
+
+#%%
+from collections import namedtuple
+City = namedtuple('City', 'name country population coordinates')
+tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
+tokyo
+
+#%%
+tokyo.population
+
+#%%
+tokyo.coordinates
+
+#%%
+tokyo[1]
+
+#%%
+City._fields
+
+#%%
+LatLong = namedtuple('LatLong', 'lat long')
+delhi_data = ('Delhi NCR', 'IN', 21.935, LatLong(28.613889, 77.208889))
+delhi = City._make(delhi_data)
+delhi._asdict()
+
+#%%
+for key, value in delhi._asdict().items():
+    print(key + ':', value)
