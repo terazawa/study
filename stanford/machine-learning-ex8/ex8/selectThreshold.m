3427b775f99b7b3f5c2ae067068abcdb4dc126d9
terazawa <tera_rothe@yahoo.co.jp>
2019-11-04 14:02:17 +0900

    ex8 1.3 select thereshold

--- stanford/machine-learning-ex8/ex8/selectThreshold.m
+++ stanford/machine-learning-ex8/ex8/selectThreshold.m
@@ -23,17 +23,12 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
-
-
-
-
-
-
-
-
-
-
-
+    tp = sum((pval(yval == 1) < epsilon));
+    fp = sum((pval(yval == 0) < epsilon));
+    fn = sum((pval(yval == 1) >= epsilon));
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
+    F1 = (2 * prec * rec) / (prec + rec);
 
     % =============================================================
 
