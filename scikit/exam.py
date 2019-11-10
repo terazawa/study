989a28ccc6b00edf385f599ac0f45b1d04ab2dbc
terazawa <tera_rothe@yahoo.co.jp>
2019-11-10 18:08:11 +0900

    1.7.1

--- scikit/exam.py
+++ scikit/exam.py
@@ -42,3 +42,39 @@ data_pandas
 
 #%%
 data_pandas[data_pandas.Age > 30]
+
+#%%
+from sklearn.datasets import load_iris
+iris_dataset = load_iris()
+print(iris_dataset)
+
+#%%
+print(iris_dataset.keys())
+
+#%%
+print(iris_dataset['DESCR'][:193])
+
+#%%
+print(iris_dataset['target_names'])
+
+#%%
+print(iris_dataset['feature_names'])
+
+#%%
+print(type(iris_dataset['data']))
+
+#%%
+print(iris_dataset['data'].shape)
+
+#%%
+print(iris_dataset['data'][:5])
+
+#%%
+print(type(iris_dataset['target']))
+
+#%%
+print(iris_dataset['target'].shape)
+
+#%%
+print(iris_dataset['target'])
+
