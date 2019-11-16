70588678cbf1598f38d2e141e7a66b92078df603
terazawa <tera_rothe@yahoo.co.jp>
2019-11-16 21:54:44 +0900

    1.7.3

--- scikit/exam.py
+++ scikit/exam.py
@@ -78,3 +78,28 @@ print(iris_dataset['target'].shape)
 #%%
 print(iris_dataset['target'])
 
+#%%
+X = iris_dataset['data']
+y = iris_dataset['target']
+print(f"X: {X.shape}")
+print(f"y: {y.shape}")
+
+#%%
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, random_state=0 )
+
+#%%
+print(f"X_train: {X_train.shape}")
+print(f"X_test : {X_test.shape}")
+print(f"y_train: {y_train.shape}")
+print(f"y_test : {y_test.shape}")
+
+#%%
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+iris_dataframe
+
+#%%
+from pandas.plotting import scatter_matrix
+grr = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+
