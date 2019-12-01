ab8fb4a851e3f0f092e0b70beae8a51734d7930b
terazawa <tera_rothe@yahoo.co.jp>
2019-12-01 17:28:25 +0900

    2.3.2.2

--- scikit/exam2.py
+++ scikit/exam2.py
@@ -109,3 +109,42 @@ chart_test = alt.Chart(df_test).mark_point(filled=False).encode(
     shape='predict:N'
 )
 chart_train + chart_test
+
+#%%
+fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+    mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=0.4)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title(f"{n_neighbors} neighbors")
+    ax.set_xlabel("feature 0")
+    ax.set_ylabel("feature 1")
+axes[0].legend(loc=3)
+
+#%%
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, 
+    cancer.target, 
+    stratify=cancer.target, random_state=66)
+
+df = pd.DataFrame(cancer.data, columns=cancer.feature_names)
+df
+
+#%%
+training_accuracy = []
+test_accuracy = []
+neighbors_settings = range(1, 11)
+for n_neighbors in neighbors_settings:
+    clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train, y_train)
+    training_accuracy.append(clf.score(X_train, y_train))
+    test_accuracy.append(clf.score(X_test, y_test))
+plt.plot(neighbors_settings, training_accuracy, label='traing accuracy')
+plt.plot(neighbors_settings, test_accuracy, label='test accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('n_neighbors')
+plt.legend()
+
+#%%
